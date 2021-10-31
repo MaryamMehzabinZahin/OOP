9707679b0ea59e1964b79989af09ee7247687064
Maryam Mehzabin Zahin <maryam.zahin.09@gmail.com>
2021-10-31 22:50:25 +0800

    code

--- classes.cpp
+++ classes.cpp
@@ -1,1 +1,34 @@
 
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+#include <cassert>
+using namespace std;
+
+// Write your Student class here
+class Student
+{
+private:
+    int scores[5];
+public:
+    void input()
+    {
+        int num;
+        for(int i=0;i<5;i++)
+        {
+            cin>>num;
+            scores[i]=num;
+        }
+    }
+    int calculateTotalScore()
+    {
+        int sum=0;
+        for(int i=0;i<5;i++)
+        {
+            sum+=scores[i];
+        }
+        return sum;
+    }
+};
